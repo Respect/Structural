a93d4f2c30d450df753352d2607fe1a40eb61fc2
Alexandre Gaigalas <alexandre@gaigalas.net>
2011-11-29 20:09:27 -0200

    Initial query generation

--- library/Respect/Structural/Mapper.php
+++ library/Respect/Structural/Mapper.php
@@ -4,10 +4,21 @@ namespace Respect\Structural;
 
 use Respect\Data\AbstractMapper;
 use Respect\Data\Collection;
+use Respect\Data\CollectionIterator;
 
 class Mapper extends AbstractMapper
 {
     protected $db;
+    
+    protected static function prefixArrayKeys(array $array, $prefix)
+    {
+        $new = array();
+        
+        foreach ($array as $key => $value)
+            $new["{$prefix}{$key}"] = $value;
+            
+        return $new;
+    }
     
     public function __construct(MongoDb $db=null) //remove null from param list TODO
     {
@@ -37,7 +48,36 @@ class Mapper extends AbstractMapper
     public function generateQuery(Collection $collection)
     {
         return array(
-            $collection->getName() => array()
+            $collection->getName() => $this->parseConditions($collection)
         );
+    }
+    
+    protected function parseConditions(Collection $collection)
+    {
+        $allCollections = CollectionIterator::recursive($collection);
+        $allCollections = iterator_to_array($allCollections);
+        $allCollections = array_slice($allCollections, 1);
+        
+        $condition = $this->getConditionArray($collection);
+        
+        foreach ($allCollections as $name => $coll) 
+            $condition += $this->getConditionArray($coll, true);
+        
+        return $condition;
+    }
+    
+    protected function getConditionArray(Collection $collection, $prefix=false)
+    {
+        $condition = $collection->getCondition();
+        
+        if (!is_array($condition)) 
+            $condition = array('_id' => $condition);
+        
+        if ($prefix)
+            $condition = static::prefixArrayKeys(
+                $condition, $collection->getName()."."
+            );
+        
+        return $condition;
     }
 }
--- tests/library/Respect/Structural/MapperTest.php
+++ tests/library/Respect/Structural/MapperTest.php
@@ -20,4 +20,24 @@ class MapperTest extends \PHPUnit_Framework_TestCase
             $result
         );
     }
+    function test_generate_query_should_return_simple_find_by_id()
+    {
+        $result = $this->mapper->generateQuery(Collection::my_coll(42));
+        $this->assertEquals(
+            array(
+                'my_coll' => array('_id' => 42)
+            ),
+            $result
+        );
+    }
+    function test_generate_query_should_use_partial_result_sets()
+    {
+        $result = $this->mapper->generateQuery(Collection::article()->author[42]);
+        $this->assertEquals(
+            array(
+                'article' => array('author._id' => 42)
+            ),
+            $result
+        );
+    }
 }
